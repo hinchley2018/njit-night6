c6d6fa41b9587c425bfba1f4bc21b1017e2028b5
Jonathon Hinchley <dev.jjh@planet-hinchley.com>
2022-04-20 21:56:11 -0400

    refactor adventure game

---
+++ adventure.js
@@ -0,0 +1,18 @@
+//prompts
+//choices
+
+let prompts = {
+    "Start Node":{
+        "prompt": "",
+        "choices": ["Left", "Right"]
+    },
+    "left": {
+        "prompt" :"You went left and saw a cat",
+        "choices": ["Stay", "Run"]
+    },
+    "right": {
+        "prompt" :"You went right and saw a dragon",
+        "choices": ["Slay Dragon", "Sneak"]
+    }
+}
+
